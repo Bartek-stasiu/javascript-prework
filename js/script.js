2a81629312f2e272b7f75e5fe3648f3a7b0e701d
Bartłomiej Musiał <musial.bartlomiej@gmail.com>
2020-01-20 23:41:18 +0100

    Add simple html and js code

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, PlayerMove;
+PlayerMove = 'papier';
+computerMove = 'kamieñ';
+printMessage('Zagra³em ' + computerMove + '! Jeœli Twój ruch to papier, to wygrywasz!' + PlayerMove);
