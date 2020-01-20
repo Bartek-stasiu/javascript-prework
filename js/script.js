f1bab66b9c6da2a2c8e03e7d90ab25af98cd9c9c
Bartłomiej Musiał <musial.bartlomiej@gmail.com>
2020-01-21 00:23:41 +0100

    Add players answers

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamieñ';
-} else if (randomNumber == '2') {
-  computerMove = 'papier';
-} else if (randomNumber == '3') {
-  computerMove = 'no¿yce';
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamieñ, 2: papier, 3: no¿yce.');
+console.log('Wpisana odpowiedŸ to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamieñ';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'no¿yce';
 } else {
-  computerMove = 'nieznany ruch';
+  playerMove = 'kamieñ';
 }
-printMessage('Mój ruch: ' + computerMove);
+printMessage('Twój ruch: ' + playerMove);
